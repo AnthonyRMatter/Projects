90faad3df251087378eef6538d1f0d8540b3854e
AnthonyRMatter <134010366+AnthonyRMatter@users.noreply.github.com>
2025-06-22 18:08:13 -0700

    Update dump.sql

--- Predictify/dump.sql
+++ Predictify/dump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
 --
--- Host: localhost    Database: Deathify
+-- Host: localhost    Database: Predictify
 -- ------------------------------------------------------
 -- Server version	8.0.31
 
